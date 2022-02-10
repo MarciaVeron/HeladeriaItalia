f7b37377815d0be6ef521e2137a9364e0587a762
MarciaVeron <90977706+MarciaVeron@users.noreply.github.com>
2022-02-10 19:19:48 -0300

    mi primer commit

--- Desktop/proyecto-react/my-app/src/App.js
+++ Desktop/proyecto-react/my-app/src/App.js
@@ -1,14 +1,41 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Home from './componentes/Home';
 import Navbar from './componentes/Navbar';
 
+
+import { BrowserRouter } from "react-router-dom";
+
+import ProductosLista from './componentes/Productos';
+/* import { Carrito } from './componentes/Carrito'; */
+
+import data from './componentes/data';
+import CarritoItem from './componentes/CarritoItem';
+
+
+
+
+
+
 function App() {
+
+  const {productos} = data;
+  const [cartItems, setCartItems] = useState([]);
+
+
   return (
-   <>
-    <Navbar/>
-    <Home/>
-   </>
+
+
+    <BrowserRouter>
+      <Navbar />
+      <Home />
+      {/* <Carrito /> */}
+      <div className='row'>
+        <ProductosLista productos={productos} />
+        <CarritoItem cartItems={cartItems}></CarritoItem>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
